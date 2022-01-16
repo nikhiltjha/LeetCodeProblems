768660a9f942a4faf27148c3ed7a740a4e6cc601
nikhiltjha <37347652+nikhiltjha@users.noreply.github.com>
2022-01-16 10:09:42 +0530

    Time: 205 ms (28.14%), Space: 42.8 MB (69.27%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cs
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cs
@@ -1,20 +1,35 @@
 public class Solution {
     public int RemoveDuplicates(int[] nums) {
         var len = nums.Length;
-        var memoryDict = new Dictionary<int,int>();
-        int j = 0;
+        if(nums.Length ==0) 
+            return 0;
+        //Dictionay help implementation
+        //var memoryDict = new Dictionary<int,int>();
+        //int j = 0;
+        //int i = 0;
+        //while(j<len)
+        //{
+        //  if(!memoryDict.ContainsKey(nums[j]))
+        //  {
+        //       nums[i]= nums[j];
+        //       memoryDict[nums[j]]=1;
+        //       i++;
+        //   }
+        //   j++; 
+        //    
+        //}
+        //return i;
+        
+        //Using the condition that it is sorted
         int i = 0;
-        while(j<len)
+        for(int j =1;j< len;j++)
         {
-           if(!memoryDict.ContainsKey(nums[j]))
-           {
-               nums[i]= nums[j];
-               memoryDict[nums[j]]=1;
-               i++;
-           }
-           j++; 
-            
+            if(nums[i]!=nums[j])
+            {
+              i++;
+              nums[i]= nums[j];  
+            }
         }
-        return i;
+        return i+1;
     }
 }
