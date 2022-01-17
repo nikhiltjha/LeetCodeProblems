fc5d85a4a8060a4e1162d0656395b3b89f22b8b4
nikhiltjha <37347652+nikhiltjha@users.noreply.github.com>
2022-01-18 00:10:52 +0530

    Time: 175 ms (9.75%), Space: 39.3 MB (72.33%) - LeetHub

--- 112-path-sum/112-path-sum.cs
+++ 112-path-sum/112-path-sum.cs
@@ -15,32 +15,32 @@ public class Solution {
 
     public bool HasPathSum(TreeNode root, int targetSum) 
     {
-        bool [] res = new bool[1];
-        res[0] = false;
+        //bool [] res = new bool[1];
+        bool res = false;
         if (root== null) return false;
         int currSum =0;
-        findPathSumMatch(root,targetSum,currSum, res);
-        return res[0];
+        findPathSumMatch(root,targetSum,currSum, ref res);
+        return res;
         
     }
-    public void findPathSumMatch(TreeNode node,int targetSum,int currSum, bool [] res)
+    public void findPathSumMatch(TreeNode node,int targetSum,int currSum, ref bool res)
     {    
         currSum = currSum + node.val;
         if(node.left == null && node.right ==null)
         { 
             if(targetSum == currSum)
-               res[0] = true;
+               res = true;
             return;
                 
         }
         if(node.left != null)
         {
-            findPathSumMatch(node.left,targetSum,currSum, res);
+            findPathSumMatch(node.left,targetSum,currSum, ref res);
         }
     
         if(node.right != null)
         {
-            findPathSumMatch(node.right,targetSum,currSum, res);
+            findPathSumMatch(node.right,targetSum,currSum, ref res);
         }
     }
 }
