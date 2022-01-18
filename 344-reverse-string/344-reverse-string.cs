30ed45730f7a0088ca2b18fe938aa933e26d4db0
nikhiltjha <37347652+nikhiltjha@users.noreply.github.com>
2022-01-18 22:56:34 +0530

    Time: 428 ms (9.02%), Space: 45.8 MB (86.87%) - LeetHub

--- 344-reverse-string/344-reverse-string.cs
+++ 344-reverse-string/344-reverse-string.cs
@@ -1,7 +1,7 @@
 public class Solution {
     public void ReverseString(char[] s) {
         var len = s.Length;
-        for(int i=0;i<len/2;i++)
+        for(int i=0;i<=(len-1)/2;i++)
         {
            var temp = s[i];
            s[i] = s[len-1-i];
