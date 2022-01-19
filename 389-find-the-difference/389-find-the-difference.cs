9905ac43ce8993f6966735fcf2506c0662a69714
nikhiltjha <37347652+nikhiltjha@users.noreply.github.com>
2022-01-19 23:28:42 +0530

    Time: 154 ms (15.20%), Space: 36.8 MB (99.02%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.cs
+++ 389-find-the-difference/389-find-the-difference.cs
@@ -1,6 +1,7 @@
 public class Solution {
     public char FindTheDifference(string s, string t) 
-    {
+    {   /*
+       //using dictionary
         var dict = new Dictionary<char,int>();
         foreach(char c in s)
         {
@@ -17,6 +18,19 @@ public class Solution {
                 dict[c] = dict[c]-1;
         }
         return ' ';
+        */
+        var len1 = s.Length;
+        if(len1==0) return t[0];
+        int c = (int)s[0] ^ (int)t[0];
+        for(int i=1;i<len1;i++)
+        {
+            c = c ^ (int)s[i] ^ (int)t[i];
+        }
+        return (char)(c ^ (int)t[len1]);
+
+
+        
+        
         
     }
 }
