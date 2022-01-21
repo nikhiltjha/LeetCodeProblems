4a6618f6439d354e038b046720e7ec779e9c43f5
nikhiltjha <37347652+nikhiltjha@users.noreply.github.com>
2022-01-21 22:33:20 +0530

    Time: 604 ms (53.68%), Space: 54.3 MB (14.70%) - LeetHub

--- 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.cs
+++ 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.cs
@@ -5,10 +5,10 @@ public class Solution {
         var sortedPoints = points.OrderBy(x=>x[1]).ToArray();
         int count = 1;
         int currHead = sortedPoints[0][1];
-        Console.WriteLine($"count :{count} currHead: {currHead}");
+        //Console.WriteLine($"count :{count} currHead: {currHead}");
         for(int i=1;i<sortedPoints.Length;i++)
         {
-            Console.WriteLine($"{sortedPoints[i][0]} {sortedPoints[i][1]}");
+            //Console.WriteLine($"{sortedPoints[i][0]} {sortedPoints[i][1]}");
             if(currHead >=sortedPoints[i][0])
                 continue;
             else
@@ -16,7 +16,7 @@ public class Solution {
                 count++;
                 currHead =sortedPoints[i][1];
             }
-            Console.WriteLine($"count :{count} currHead: {currHead}");
+            //Console.WriteLine($"count :{count} currHead: {currHead}");
         }
         return count;
         
